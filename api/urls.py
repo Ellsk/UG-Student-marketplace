888ddb22db0b643803fdf59297829b50150ed0f7
Elliot Kumedzro <ebkumedzro@st.ug.edu.gh>
2024-08-07 13:57:29 +0000

    Added login page with some adjustments on <head> tag

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ from api.views import index
 app_name = "api" # For demo purpose api is used, once app name is decided upon needs to be here
 
 urlpatterns = [
-    path("", index)
+    path("", index, name="index")
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 def register_view(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('userauths:sign-up-success')  # Redirect to a success page or login page
+        if form.is_valid(): 
+            new_user = form.save()
+            id = form.cleaned_data.get("id")
+            messages.success(request, f"Hey {id}, Your account was created successfully ")
+            new_user = authenticate(username =form.cleaned_data['pin'],
+                                    password=form.cleaned_data['password1']
+            ) 
+            login(request, new_user)
+            return redirect('api:index')  # Redirect to a success page or login page
     else:
-        form = UserRegisterForm()
+        form = UserRegisterForm() 
 
-    return render(request, 'userauths/sign-up.html', {'form': form})
+    context = {
+        'form': form,
+    }
+    return render(request, 'userauths/sign-up.html', context)

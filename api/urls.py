9c28d8a45bd6d20f25f7d31b00f9762cca5617bd
Elliot Kumedzro <ebkumedzro@st.ug.edu.gh>
2024-08-10 20:05:59 +0000

    Product List Category View

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path 
 from api import views
-from api.views import index, product_list_view, category_list_view  
+from api.views import index, product_list_view, category_list_view , category_product_list_view
 
 
 app_name = "api"
 
+
 urlpatterns = [
     path("", index, name="index"),
     path("products/", product_list_view, name="product-list"),
-    path("category/", category_list_view, name="category-list")
+    path("category/", category_list_view, name="category-list"),
+    path("category/<cid>/", category_product_list_view, name="category-product-list"),
 ]
--- api/views.py
+++ api/views.py
@@ -25,7 +25,7 @@ def product_list_view(request):
     
     return render(request, "core/product-list.html", context)
 
-def category_list_view(request):
+def category_list_view(request):    
     categories = Category.objects.all()
     
     context = {
@@ -33,3 +33,59 @@ def category_list_view(request):
     }
     
     return render(request, "core/category-list.html", context)
+
+def category_product_list_view(request, cid):
+    category = Category.objects.get(cid=cid)
+    products = Product.objects.filter(category=category, product_status="published") 
+    
+    context = {
+        "category": category,
+        "products": products
+    }
+    
+    from django.http import HttpResponse
+from django.shortcuts import render
+from api.models import Product, ProductImages, ProductReview, Wishlist, Address, CartOrder, Category, Vendor, CartOrderItems
+# Create your views here.
+
+def index(request):
+    #For displaying latest products
+    #product = Product.objects.all().order_by("-id")
+    
+    product = Product.objects.filter(featured=True, product_status="published")
+    
+    context = {
+        "products": product
+    }
+    
+    return render(request, "core/index.html", context)
+
+def product_list_view(request):
+    #Whether the product is featured or not as long as it's published we will display
+    product = Product.objects.filter(product_status="published")
+    
+    context = {
+        "products": product
+    }
+    
+    return render(request, "core/product-list.html", context)
+
+def category_list_view(request):    
+    categories = Category.objects.all()
+    
+    context = {
+        "categories": categories
+    }
+    
+    return render(request, "core/category-list.html", context)
+
+def category_product_list_view(request, cid):
+    category = Category.objects.get(cid=cid)
+    products = Product.objects.filter(category=category, product_status="published") 
+    
+    context = {
+        "category": category,
+        "products": products
+    }
+    
+    return render(request, "core/category-product-list.html", context)

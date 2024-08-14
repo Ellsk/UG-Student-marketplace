5a8c5629573102ddf0e8cb55c62d6591ac175502
Elliot Kumedzro <ebkumedzro@st.ug.edu.gh>
2024-08-14 19:17:13 +0000

    Filter product by category and vendor

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -61,17 +61,18 @@ $("#commentForm").submit(function(e){
 $(document).ready(function(){
     $(".loader").hide()
     $(".filter-checkbox").on("click", function(){
-    let filter_object = {}
-    $(".filter-checkbox").each(function(index){
-    let filter_value = $(this).val()
-    let filter_key = $(this).data("filter")
-    console.log(filter_value, filter_key);
-    filter_object[filter_key] =
-    Array.from(document.querySelectorAll('input[data-filter='+filter_key+']:checked')).map(function
-    (element){
-    return element.value
-    })
-    })
-    console.log(filter_object);
+    
+        let filter_object = {}
+
+        $(".filter-checkbox").each(function(index){
+            let filter_value = $(this).val()
+            let filter_key = $(this).data("filter") // vendor or category
+            console.log(filter_value, filter_key);
+            
+            filter_object[filter_key] = Array.from(document.querySelectorAll('input[data-filter='+filter_key+']:checked')).map(function(element){
+                return element.value
+            })
+        })
+        console.log(filter_object);
     })
 })
